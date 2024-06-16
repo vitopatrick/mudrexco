527209552d64a754042c5091aec5b96b284e6780
warlock <fecypher@gmail.com>
2024-06-16 10:16:48 +0100

    wll

--- lib/wallet-address.ts
+++ lib/wallet-address.ts
@@ -18,14 +18,14 @@ export const walletOne = [
 export const walletTwo = [
   {
     id: 1,
-    address: "bc1qdruu02pf0ayhzqvgfsf840mr45lwe5dsmv59gm",
+    address: "114t8CWbEM41y1mB7DyXcBEDukkToCLyvH",
     sym: "BTC",
     name: "bitcoin",
     img: "/wallets/btc.jpeg",
   },
   {
     id: 2,
-    address: "0xe0741670B8a64Ebc652a8414198c74D3C5c55b62",
+    address: "0x26b5e2ccc63e2695418f7db7f4a5e88dc60e4d27",
     sym: "ETH",
     name: "Ethereum",
     img: "/wallets/eth.jpeg",
