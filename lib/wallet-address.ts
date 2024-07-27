37ffbed6007ca00d7bf788b532ef0fd62eb06d89
warlock <fecypher@gmail.com>
2024-07-27 19:48:06 +0100

    fixed

--- lib/wallet-address.ts
+++ lib/wallet-address.ts
@@ -1,4 +1,39 @@
-export const addresses = [
+export const walletOne = [
+  {
+    id: 2,
+    sym: "erc20",
+    name: "Ethereum",
+    address: "0xc4e43bb957cfc4e8f2c80a104d0c3041ab3729d4",
+    network: "Eth",
+    img: "",
+  },
+  {
+    id: 3,
+    sym: "usdt",
+    name: "Tether ",
+    address: "TZBFHCXXfjaSybhkFQ3T315sbEAxnxvmdq",
+    network: "usdt",
+    img: "",
+  },
+  {
+    id: 1,
+    sym: "btc",
+    name: "Bitcoin",
+    address: "3QZ4CNpHSWMnG4P7LzX72gxHrQFpDr4Etx",
+    network: "BTC",
+    img: "",
+  },
+  {
+    id: 4,
+    sym: "ltc",
+    name: "Litecoin",
+    address: "LRAKP6FZ8YLtJd9tcy4dzbsSBk6u4FHDTT",
+    network: "litecoin",
+    img: "",
+  },
+];
+
+export const walletTwo = [
   {
     id: 2,
     sym: "erc20",
